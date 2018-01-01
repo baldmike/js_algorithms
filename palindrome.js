cb8c23445a3cca888eda8047a5953a80402301e9
Bald Mike <baldmike312@gmail.com>
2017-12-31 22:12:13 -0600

    tidy3

--- palindrome.js
+++ palindrome.js
@@ -10,10 +10,26 @@ function isPal(str){
     return true;  
 };
 
-
-
-
-
 isPal ("ottox");
 
 
+// alternate way
+
+function pal(word) {
+    j = word.length-1;
+    isPal = false;
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] == word[j]) {
+            isPal = true;
+            j--
+        } else {
+            isPal = false;
+            console.log(isPal + "! This ain't no PALINDROME.");
+            return isPal
+        }
+    }
+    console.log(isPal + "! This is, by all means, god dammit, a fricken PALINDROME.");
+    return isPal
+}
+
+pal("kissing777gnissik")
