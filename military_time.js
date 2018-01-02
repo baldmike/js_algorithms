f2c67d7166e7f26a83f883a573f62a4a17228a8e
Bald Mike <baldmike312@gmail.com>
2018-01-02 02:08:17 -0600

    military time works

--- military_time.js
+++ military_time.js
@@ -1,13 +1,19 @@
 
-function military_time(str) {
-    var m = str[str.length-2];
-    if (m === "P") {
-        var x = String(Number(str[0]) + 1) + String(Number(str[1]) + 2);
-        var y = str.slice(0, 8).slice(2, 8)
-        var z = x + y
+function military_time(s) {
+    var m = s[s.length-2];
+    if (m === "A" && (Number(s[0]) + Number(s[1])) === 3) {
+        var x = String(Number(s[0]) - 1) + String(Number(s[1]) - 2);
+        var y = s.slice(0, 8).slice(2, 8);
+        var z = x + y;
+    } else if (m === "P" && (Number(s[0]) + Number(s[1]) === 3)) {
+        z = s.slice(0, 8);
+    } else if (m === "P") {
+        var x = String(Number(s[0]) + 1) + String(Number(s[1]) + 2);
+        var y = s.slice(0, 8).slice(2, 8);
+        var z = x + y;
     } else {
-        z = str.slice(0, 8)
-    } console.log(str + " is " + z + " in military time.")
+        z = s.slice(0, 8)
+    } console.log(s + " is " + z + " in military time.")
 }
 
-military_time("10:14:45PM")
+military_time("12:59:45AM")
