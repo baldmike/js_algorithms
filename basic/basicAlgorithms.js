103c6a3a305963e828ce505eb06c22eb78c5d37a
Bald Mike <baldmike312@gmail.com>
2018-01-09 13:50:25 -0600

    reverse

--- basic/basicAlgorithms.js
+++ basic/basicAlgorithms.js
@@ -86,3 +86,16 @@ function compareFC() {
 }
 
 compareFC()
+
+function reverse(arr) {
+    var temp = [];
+    for (var i = arr.length-1; i >= 0 ; i--) {
+        temp.push(arr[i]);
+    } 
+    arr = temp;
+    console.log(arr);
+}
+
+arr = [5,4,3,2,1]
+reverse(arr)
+
